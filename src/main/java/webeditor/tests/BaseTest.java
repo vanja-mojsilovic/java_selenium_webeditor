7f9324c0088ace1d714e1e4ae31fea023f2fa3aa
vanja-mojsilovic <vanja.mojsilovic@spothopperapp.com>
2025-08-26 11:41:52 +0200

    headless

--- src/main/java/webeditor/tests/BaseTest.java
+++ src/main/java/webeditor/tests/BaseTest.java
@@ -16,6 +16,7 @@ public abstract class BaseTest {
         ChromeOptions options = new ChromeOptions();
 
         if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--window-size=1920,1080");

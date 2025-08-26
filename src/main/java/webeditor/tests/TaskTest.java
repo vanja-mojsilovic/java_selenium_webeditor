db47e66f436a9e803909a834afa1b9ab21df4cfa
vanja-mojsilovic <vanja.mojsilovic@spothopperapp.com>
2025-08-26 10:28:02 +0200

    project web erased again

--- src/main/java/webeditor/tests/TaskTest.java
+++ src/main/java/webeditor/tests/TaskTest.java
@@ -32,7 +32,7 @@ public class TaskTest extends BaseTest{
 
     // Methods
     public void searchTasks_1() throws InterruptedException, IOException {
-        String jqlBuildsFilter = "project = WEB AND summary ~ 'Go Live' AND status = QA";
+        String jqlBuildsFilter = "summary ~ 'Go Live' AND status = QA";
         System.out.println("jqlBuildsFilter: " + jqlBuildsFilter);
 
         WebEditorPage webEditorPage = new WebEditorPage(driver);
@@ -40,7 +40,7 @@ public class TaskTest extends BaseTest{
         String allKeyIssues = webEditorPage.getKeyIssuesByApiPost(jqlBuildsFilter, variablesPage.emailGoogle, variablesPage.jiraApiKey);
         allKeyIssues = "issue in (" + allKeyIssues + ")";
         System.out.println("allKeyIssues: " + allKeyIssues);
-        String wasInQaTasks = "project = WEB AND summary ~ \"Go Live\" AND status = QA AND comment ~ \"\\\"Please check if there is a Web editor task on this branch!\\\"\"";
+        String wasInQaTasks = "summary ~ \"Go Live\" AND status = QA AND comment ~ \"\\\"Please check if there is a Web editor task on this branch!\\\"\"";
         
         webEditorPage.navigate(variablesPage.jiraFilterPageUrl);
         webEditorPage.enterJql(wasInQaTasks);
@@ -103,73 +103,7 @@ public class TaskTest extends BaseTest{
     }
 
 
-    public void searchTasks() throws InterruptedException, IOException {
-        
-        String jqlBuildsFilter = "summary ~ 'Go Live' AND status = QA";
-        System.out.println("jqlBuildsFilter: " + jqlBuildsFilter);
-
-        WebEditorPage webEditorPage = new WebEditorPage(driver);
-        VariablesPage variablesPage=new VariablesPage(driver);
-        String allKeyIssues = webEditorPage.getKeyIssuesByApiPost(jqlBuildsFilter, variablesPage.emailGoogle,variablesPage.jiraApiKey);
-        allKeyIssues = "issue in (" + allKeyIssues + ")";
-        System.out.println("allKeyIssues: " + allKeyIssues);
-
-        String wasInQaTasks = "summary ~ \"Go Live\" AND status = QA AND comment ~ \"Please check if there is a Web editor task on this branch!\"";
-       
-        String wasInQaIssuesSeparatedWithCommas = webEditorPage.getKeyIssuesByApiPost(wasInQaTasks, variablesPage.emailGoogle, variablesPage.jiraApiKey);
-
-        String excludedTask = "";
-        if (!wasInQaIssuesSeparatedWithCommas.trim().equals("")) {
-            excludedTask += " AND issue not in (" + wasInQaIssuesSeparatedWithCommas + ")";
-        }
-
-        String resultTasks = allKeyIssues + excludedTask;
-        System.out.println("resultTasks: " + resultTasks);
-
-        String encodedJql = URLEncoder.encode(resultTasks, "UTF-8");
-        String apiQueryUrl = "https://spothopper.atlassian.net/rest/api/3/search?jql=" + encodedJql + "&maxResults=1000";
-
-        numberOfTasks = webEditorPage.getIssueKeyParentIsssueKeyFromApi(driver, apiQueryUrl, issueKeyCollection, parentIssueKeyCollection);
-        System.out.println("numberOfTasks: " + numberOfTasks);
-
-        if (numberOfTasks == 0) {
-            System.out.println("numberOfTasks == 0");
-            //driver.close();
-            return;
-        }
-
-        for (int i = 0; i < numberOfTasks; i++) {
-            String issueKey = issueKeyCollection.get(i);
-            String parentIssueKey = parentIssueKeyCollection.get(i);
-            encodedJql = "parent%20%3D%20" + URLEncoder.encode(parentIssueKey, StandardCharsets.UTF_8);
-            apiQueryUrl = "https://spothopper.atlassian.net/rest/api/3/search?jql=" + encodedJql+"&maxResults=1000";
-            System.out.println("apiQueryUrl: " + apiQueryUrl);
-            webEditorPage.navigate(apiQueryUrl);
-            Thread.sleep(1000);
-            List<String> childIssuesSummariesList = new ArrayList<String>();
-            List<String> childIssuesKeyList = new ArrayList<String>();
-            List<String> childIssuesStatusList = new ArrayList<String>();
-            boolean hasWebsiteEditorInProgress = webEditorPage.getChildTasksSummaries(driver, apiQueryUrl,childIssuesSummariesList,childIssuesKeyList,childIssuesStatusList);
-            for(int j=0;j<childIssuesSummariesList.size();j++) {
-                System.out.println((j+1)+". "+childIssuesSummariesList.get(j) +" : "+
-                    childIssuesKeyList.get(j) +" : "+
-                    childIssuesStatusList.get(j));
-            }
-            logFileMessage = (counter+1)+". "+issueKey;
-            if(hasWebsiteEditorInProgress) {
-                String logMessage = " >>>>> Issue found! <<<<< " + issueKey;
-                logFileMessage += logMessage;
-                System.out.println(logMessage);
-                String commentMessage = "Please check if there is a Web editor task on this branch!";
-                //webEditorPage.enterNewJiraComment(driver,issueKey,commentMessage);
-                
-            }
-            counter++;
-            System.out.println("logFileMessage: " + logFileMessage);
-            System.out.println(" -- end of the task --");
-            
-        }
-    }
+    
 
     
 
